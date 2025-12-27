8602e65de1334635c44ae2d35a2039b9f44589a5
Jacob <jwm3926@gmail.com>
2025-12-26 23:13:07 -0500

    created

---
+++ Quiz.py
@@ -0,0 +1,12 @@
+def quiz():
+    question_1 = (input("What Day is our anniversary?"))
+    question_2 = (input("What was my first dog's name?"))
+    question_3 = (input("What show are we currently watching?"))
+    a1 = ("December 30th")
+    a2 = ("Sadie")
+    a3= ("It's Always Sunny in Philadelphia")
+
+
+
+quiz
+
